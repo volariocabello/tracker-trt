a7671b2c6509c3adbc57285607003c6d7537cbec
Volario <volariocabello@proton.me>
2023-03-10 04:34:29 +0200

    commit 4

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import "@/styles/globals.css";
-import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }) {
-	return (
-		<>
-			<Navbar />
-			<Component {...pageProps} />
-		</>
-	);
+	return <Component {...pageProps} />;
 }
